c22204730ffb80fe226c1dcb79c413a1aa873ef7
Ulises <ulises.mor.santos@gmail.com>
2026-01-06 18:47:37 -0700

    First Commit

---
+++ src/main/java/com/IndustrialDataAPI/IndustrialDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.IndustrialDataAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IndustrialDataAPI/IndustrialDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IndustrialDataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
