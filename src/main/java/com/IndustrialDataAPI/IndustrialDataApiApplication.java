605628d8dae169c0c0015144a51ae38274fafc5b
Ulises <ulises.mor.santos@gmail.com>
2026-01-07 01:48:24 -0700

    Configuration of log4j2 and LogConfig class

--- src/main/java/com/IndustrialDataAPI/IndustrialDataApiApplication.java
+++ src/main/java/com/IndustrialDataAPI/IndustrialDataApiApplication.java
@@ -2,8 +2,10 @@ package com.IndustrialDataAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class IndustrialDataApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/common/LogConfig.java
@@ -0,0 +1,15 @@
+package common;
+
+import org.apache.logging.log4j.ThreadContext;
+import java.util.UUID;
+
+public class LogConfig {
+
+    private static final String COMPONENT = "component";
+    private static final String TRANSACTION_ID = "transactionId";
+
+    public static void initLog4j2(){
+        ThreadContext.put(COMPONENT, App.APPLICATION_NAME);
+        ThreadContext.put(TRANSACTION_ID, UUID.randomUUID().toString());
+    }
+}
